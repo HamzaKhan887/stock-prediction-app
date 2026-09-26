9f02f7d654bec0321650b2273e115a5f69b207ef
Hamza Khan <hamza.4.khan@kcl.ac.uk>
2026-09-26 03:24:56 +0100

    Update dashboard search form image

--- react-frontend/src/pages/Dashboard.tsx
+++ react-frontend/src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ function Dashboard() {
   const { mutate, data, isPending } = useStockAnalysis()
   return (
     <main
-      className={`page-center items-center px-5 ${data ? 'mt-10 mb-5' : ''}`}
+      className={`page-center items-center px-5 ${data ? 'mt-5 mb-5' : ''}`}
     >
       <SearchForm onSearch={mutate} isPending={isPending} />
       {data && <PredictionResults data={data} />}
